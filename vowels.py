eb16a46f019044ed07eba1be8a575fbe8cbaf595
rhtshahi <108247838+rhtshahi@users.noreply.github.com>
2022-07-27 09:46:41 +0545

    Update vowels.py

--- vowels.py
+++ vowels.py
@@ -1,7 +1,7 @@
 #----------------------Progtam to count vowels of user input string----------------------#
 
 x=input('Enter any word: ')#taking user input
-y=[i for i in x if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u')]#condition to take vowels as list
+y=[i for i in x if(i=='a' or i=='e' or i=='i' or i=='o' or i=='u' or i=='A' or i=='E' or i=='I' or i=='O' or i=='U')]#condition to take vowels as list
 #print(y)
 v=len(y)#length of list
 #print(v)
